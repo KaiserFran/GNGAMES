59de503e3b55dd835bfb6f40528456312aab3562
KaiserFran <obregon.franco2002@gmal.com>
2025-08-15 13:02:22 -0300

    cambio de db a Mysql

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -9,11 +9,23 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# --- Static files ---
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # carpeta con tus CSS, JS, imágenes originales
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # aquí se guardarán los estáticos recolectados
+
+# --- Media files ---
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"  # aquí se guardarán los archivos subidos por usuarios
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,8 +88,12 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyecto',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # o la IP del servidor
+        'PORT': '3306',        # puerto por defecto de MySQL
     }
 }
 
